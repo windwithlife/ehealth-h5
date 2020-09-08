ccfc11af4267c6a12a559f0893945a61e4c779d0
fzhange <2861669535@qq.com>
2020-09-08 22:28:41 +0800

    feat: 倒计时新增

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,7 @@ class MyApp extends App {
             <>
                  <Head>
                     <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0,viewport-fit=cover" />
                 </Head>
                 <Provider {...this.mobxStore}>
                     <Component {...ComponentInitProps}/>
--- pages/live/index.js
+++ pages/live/index.js
@@ -1,118 +1,126 @@
 import React from 'react';
 import "./index.less";
 import InfoAdd from "./InfoAdd"
-import {invoke_post} from "../../common/index"
+import { invoke_post } from "../../common/index"
 import { Modal } from "antd-mobile";
-import {judgeClient} from "../../common/tools";
+import { judgeClient } from "../../common/tools";
+import Countdown from 'react-countdown';
 
 
 
 
-export default class Live extends React.Component{
-  static async getInitialProps({router}) {
+export default class Live extends React.Component {
+  static async getInitialProps({ router }) {
     return { router };
   }
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      whichTap:"LEFT", //LEFT | RIGHT
-      initData : {},
-      isShowInfoAdd:false,
+      whichTap: "LEFT", //LEFT | RIGHT
+      initData: {},
+      isShowInfoAdd: false,
+      countdownTime: null,
     }
   }
-  isShowInfoAddCall(value){
-    this.setState({isShowInfoAdd:value})
+  isShowInfoAddCall(value) {
+    this.setState({ isShowInfoAdd: value })
   }
-  async init(){
-    try{
-      let {query} = this.props.router;
-      let data = await invoke_post('advertService/getLiveDetail',{
-        id:query?.id
-      }).then(result=>result?.data)
-      this.setState({initData:data},()=>{
-        this.loadPlayer(data); 
+  async init() {
+    try {
+      let { query } = this.props.router;
+      let data = await invoke_post('advertService/getLiveDetail', {
+        id: query?.id
+      }).then(result => result?.data)
+      this.setState({ initData: data }, () => {
+        this.loadPlayer(data);
       })
-    }catch(error){
+    } catch (error) {
       console.error('error: ', error);
     }
   }
-  async componentDidMount(){
+  async componentDidMount() {
     this.init();
   }
 
-  tapClick(whichTap){
-    this.setState({whichTap})
+  tapClick(whichTap) {
+    this.setState({ whichTap })
   }
-  androidLivePlay(data){
-    try{
-      const {pullFlvUrl} = data;
-      import("flv.js").then((flvjsData)=>{
+  androidLivePlay(data) {
+    try {
+      const { pullFlvUrl } = data;
+      import("flv.js").then((flvjsData) => {
         let flvjs = flvjsData.default;
-        console.log('flv.isSupported(): ', flvjs.isSupported(),pullFlvUrl);
+        console.log('flv.isSupported(): ', flvjs.isSupported(), pullFlvUrl);
         if (flvjs.isSupported()) {
           var videoElement = document.getElementById('videoElement');
           var flvPlayer = flvjs.createPlayer({
-            type : "flv",
-            url : pullFlvUrl,
+            type: "flv",
+            url: pullFlvUrl,
           });
           flvPlayer.attachMediaElement(videoElement);
           flvPlayer.load();
           flvPlayer.play();
-          flvPlayer.on('error',function(error){
+          flvPlayer.on('error', function (error) {
             console.error('flv_error: ', error);
           })
-        }else{
-          Modal.alert('提示', "设备不支持flv流媒体格式", [{text: '确定',onPress: ()=>{}}])
+        } else {
+          Modal.alert('提示', "设备不支持flv流媒体格式", [{ text: '确定', onPress: () => { } }])
         }
       })
-    }catch(error){
+    } catch (error) {
       console.error('error: ', error);
     }
   }
-  iosLivePlay(data){
-    try{
-      const {pullHlsUrl} = data;
-      import("hls.js").then((hlsData)=>{
+  iosLivePlay(data) {
+    try {
+      const { pullHlsUrl } = data;
+      import("hls.js").then((hlsData) => {
         let Hls = hlsData.default;
-        console.log('Hls.isSupported(): ', Hls.isSupported(),pullHlsUrl);
+        console.log('Hls.isSupported(): ', Hls.isSupported(), pullHlsUrl);
         let video = document.getElementById('videoElement');
         let videoSrc = pullHlsUrl;
         if (Hls.isSupported()) {
           let hls = new Hls();
           hls.loadSource(videoSrc);
           hls.attachMedia(video);
-          hls.on(Hls.Events.MANIFEST_PARSED, function() {
+          hls.on(Hls.Events.MANIFEST_PARSED, function () {
             video.play();
           });
-        }else if(video.canPlayType('application/vnd.apple.mpegurl')){
+        } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
           video.src = videoSrc;
-          video.addEventListener('loadedmetadata', function() {
+          video.addEventListener('loadedmetadata', function () {
             video.play();
           });
-        }else{
-          Modal.alert('提示', "设备不支持hls流媒体格式", [{text: '确定',onPress: ()=>{}}])
+        } else {
+          Modal.alert('提示', "设备不支持hls流媒体格式", [{ text: '确定', onPress: () => { } }])
         }
-      }).catch((error)=>{
+      }).catch((error) => {
         console.error('hls——error: ', error);
       })
-    }catch(error){
+    } catch (error) {
       console.error('error: ', error);
     }
   }
-  loadPlayer(data){ 
-    const {videoMp4Url,roomStatus,publishStatus} = data;
-    if(roomStatus == 1){
-      if(judgeClient() == "IOS") this.iosLivePlay(data);
+  countdownComplete(){
+    location.reload();
+  }
+  loadPlayer(data) {
+    const { videoMp4Url, roomStatus, publishStatus, liveStartDate } = data;
+    console.log('liveStartDate: ', liveStartDate);
+    if (roomStatus == 1) {
+      if (judgeClient() == "IOS") this.iosLivePlay(data);
       else this.androidLivePlay(data)
-    }else if(roomStatus == 2 && publishStatus == 1){
+    } else if (roomStatus == 2 && publishStatus == 1) {
       let video = document.getElementById('videoElement');
       video.src = videoMp4Url;
+    } else if (roomStatus == 0) {
+      this.setState({ countdownTime: liveStartDate })
     }
   }
 
 
-  render(){
-    const {whichTap,initData,isShowInfoAdd} = this.state;
+  render() {
+    const { whichTap, initData, isShowInfoAdd, countdownTime } = this.state;
 
     const info_con_top_module = (
       <div className="info_con_top">
@@ -123,26 +131,26 @@ export default class Live extends React.Component{
           &nbsp;{initData.playNumber}
         </div>
         <div>
-          {initData.roomStatus==1 && "直播中"}
-          {initData.roomStatus==0 && `${initData.liveStartDate} 开播`}
-          {initData.roomStatus==2 && initData.playAble == 1 && initData.publishStatus == 0 && "直播结束，回放视频上传中"}
-          {initData.roomStatus==2 && (initData.playAble == 0 || initData.publishStatus == 2) && "直播已结束"}
+          {initData.roomStatus == 1 && "直播中"}
+          {initData.roomStatus == 0 && `${initData.liveStartDate} 开播`}
+          {initData.roomStatus == 2 && initData.playAble == 1 && initData.publishStatus == 0 && "直播结束，回放视频上传中"}
+          {initData.roomStatus == 2 && initData.publishStatus == 1 && "直播结束，查看回放"}
         </div>
       </div>
     )
     let info_con_bottom_module = null;
-    if(Object.keys(initData).length){
+    if (Object.keys(initData).length) {
       info_con_bottom_module = (
         <div className="info_con_bottom">
           <>
-            <div onClick={this.tapClick.bind(this,"LEFT")}
-              style={whichTap=="LEFT"?{background:"#108ee9",color:"#fff"}:{}} 
+            <div onClick={this.tapClick.bind(this, "LEFT")}
+              style={whichTap == "LEFT" ? { background: "#108ee9", color: "#fff" } : {}}
               className="info_con_bottom_left_tap tap">
               会议日程
             </div>
-            <div  onClick={this.tapClick.bind(this,"RIGHT")}
-            style={whichTap=="RIGHT"?{background:"#108ee9",color:"#fff"}:{}}
-            className="info_con_bottom_right_tap tap">
+            <div onClick={this.tapClick.bind(this, "RIGHT")}
+              style={whichTap == "RIGHT" ? { background: "#108ee9", color: "#fff" } : {}}
+              className="info_con_bottom_right_tap tap">
               会议介绍
             </div>
             <img className="desc_img" src={
@@ -158,9 +166,19 @@ export default class Live extends React.Component{
         {
           !isShowInfoAdd && (
             <>
-              <video id="videoElement"  controls >
-                Your browser is too old which does not support HTML5 video.
-              </video>
+              <div className="video_con">
+                <video id="videoElement" controls >
+                  Your browser is too old which does not support HTML5 video.
+                </video>
+                {
+                  !!countdownTime && (
+                    <div className="count_down_con">
+                      <Countdown onComplete={this.countdownComplete.bind(this)} date={'2020-09-08 22:30:35'}></Countdown>
+                    </div>
+                  )
+                }
+
+              </div>
               <div className="info_con">
                 {info_con_top_module}
                 {info_con_bottom_module}
@@ -168,7 +186,7 @@ export default class Live extends React.Component{
             </>
           )
         }
-        <InfoAdd isShowInfoAddCall={this.isShowInfoAddCall.bind(this)}></InfoAdd>
+        {/* <InfoAdd isShowInfoAddCall={this.isShowInfoAddCall.bind(this)}></InfoAdd> */}
       </div>
     )
   }
